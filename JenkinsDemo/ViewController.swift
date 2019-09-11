eb10485f7d3f615689aa7da20d1a9ec47bfc3527
chiyuanwei <chiyuanwei@bitnei.cn>
2019-09-11 16:37:45 +0800

    exportOptionsPlist路径修改

--- JenkinsDemo/ViewController.swift
+++ JenkinsDemo/ViewController.swift
@@ -18,10 +18,19 @@ class ViewController: UIViewController {
 //    CODE_SIGN_IDENTITY = iPhone Developer: Peng Liu (P5BW3L25TH)
     
     //:completeSettings = none
-
-//    ++ /usr/libexec/PlistBuddy -c 'Print CFBundleShortVersionString' JenkinsDemo/JenkinsDemo/Resouce/Info.plist
-//    Print: Entry, "CFBundleShortVersionString", Does Not Exist
-//    + __BUNDLE_VERSION='File Doesn'\''t Exist, Will Create: JenkinsDemo/JenkinsDemo/Resouce/Info.plist'
- 
+//    + echo '================= Clean一下 ================='
+//    ================= Clean一下 =================
+//    + xcodebuild clean -workspace SCHEME_NAME.xcworkspace -scheme SCHEME_NAME -configuration Debug
+//    xcodebuild: error: 'SCHEME_NAME.xcworkspace' does not exist.
+//
+//    Build settings from command line:
+//    CFBundleVersion = 1
+//    CODE_SIGN_IDENTITY = iPhone Developer: Peng Liu (P5BW3L25TH)
+//
+//    xcodebuild: error: 'JenkinsDemo.xcworkspace' does not exist.
+//    Build step 'Execute shell' marked build as failure
+//
+//    11_16_32_28/JenkinsDemo.xcarchive -exportPath /Users/Wynter/Desktop/Archive/JenkinsDemo_2019-09-11_16_32_28 -destination generic/platform=ios -exportOptionsPlist JenkinsDemo/BeijingXianDaiTest.plist -allowProvisioningUpdates
+//    error: Couldn't load -exportOptionsPlist: The file “BeijingXianDaiTest.plist” couldn’t be opened because there is no such file.
 }
 
