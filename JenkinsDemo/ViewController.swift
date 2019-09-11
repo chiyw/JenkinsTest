d8e850daa59376a7f786428f6ea48c4f282cc70d
chiyuanwei <chiyuanwei@bitnei.cn>
2019-09-11 13:48:33 +0800

    jenkins测试初始化项目

---
+++ JenkinsDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JenkinsDemo
+//
+//  Created by zzz on 2019/9/11.
+//  Copyright © 2019 com.bitnei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
