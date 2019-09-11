86fe73456764a2931d273f2056c05a21b29921fb
chiyuanwei <chiyuanwei@bitnei.cn>
2019-09-11 16:10:07 +0800

    info.plist路径修改

--- JenkinsDemo/ViewController.swift
+++ JenkinsDemo/ViewController.swift
@@ -14,7 +14,14 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    //:configuration = Debug
+//    CODE_SIGN_IDENTITY = iPhone Developer: Peng Liu (P5BW3L25TH)
+    
+    //:completeSettings = none
 
-
+//    ++ /usr/libexec/PlistBuddy -c 'Print CFBundleShortVersionString' JenkinsDemo/JenkinsDemo/Resouce/Info.plist
+//    Print: Entry, "CFBundleShortVersionString", Does Not Exist
+//    + __BUNDLE_VERSION='File Doesn'\''t Exist, Will Create: JenkinsDemo/JenkinsDemo/Resouce/Info.plist'
+ 
 }
 
